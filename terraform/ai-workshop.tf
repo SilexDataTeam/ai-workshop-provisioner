7ae6ae3dd5f917a85cef8a79a9bdc6cd007695e2
Derek Lewis <7467911+derekelewis@users.noreply.github.com>
2024-10-12 11:53:06 -0500

    change kubernetes auth

--- terraform/ai-workshop.tf
+++ terraform/ai-workshop.tf
@@ -296,7 +296,11 @@ provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.ai_workshop_eks_cluster.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.ai_workshop_eks_cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.ai_workshop_eks_cluster_auth.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.ai_workshop_eks_cluster.name]
+      command     = "aws"
+    }
   }
 }
 
