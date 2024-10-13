7bf4bcc661c59297c75078c95a47117e999d3327
Derek Lewis <7467911+derekelewis@users.noreply.github.com>
2024-10-12 20:37:03 -0500

    refactor into modules

--- terraform/eks-config/variables.tf
+++ terraform/eks-config/variables.tf
@@ -1,1 +1,63 @@
+variable "number_of_users" {
+  type    = number
+  default = 30
+}
 
+variable "user_prefix" {
+  type    = string
+  default = "user"
+}
+
+variable "ai_workshop_eks_cluster_name" {
+  type = string
+}
+
+variable "cluster_ca_certificate" {
+  type = string
+}
+
+variable "cluster_autoscaler_iam_role_arn" {
+  type = string
+}
+
+variable "load_balancer_controller_iam_role_arn" {
+  type = string
+}
+
+variable "cluster_endpoint" {
+  type = string
+}
+
+variable "ai_workshop_domain_name" {
+  type = string
+}
+
+variable "ai_workshop_route53_zone_name" {
+  type = string
+}
+
+variable "ai_workshop_docker_registry_username" {
+  description = "The docker registry username for the EKS cluster."
+  type        = string
+}
+
+variable "ai_workshop_docker_registry_password" {
+  description = "The docker registry password for the EKS cluster."
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_private_key" {
+  description = "The SSH private key for GitHub deploy key."
+  type        = string
+  sensitive   = true
+}
+
+variable "ai_workshop_shared_password" {
+  type = string
+}
+
+variable "ai_workshop_materials_git_repo_url" {
+  description = "The git repo url for the repo that contains the workshop materials."
+  type        = string
+}
