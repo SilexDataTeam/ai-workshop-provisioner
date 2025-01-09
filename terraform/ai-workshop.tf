b2d998a7913914ef18cf58051c090bd97ec77397
Derek Lewis <7467911+derekelewis@users.noreply.github.com>
2025-01-09 16:41:36 -0600

    inject number_of_users

--- terraform/ai-workshop.tf
+++ terraform/ai-workshop.tf
@@ -20,4 +20,5 @@ module "ai_workshop_config" {
   ai_workshop_shared_password           = var.ai_workshop_shared_password
   ai_workshop_materials_git_repo_url    = var.ai_workshop_materials_git_repo_url
   openai_api_key                        = var.openai_api_key
+  number_of_users                       = var.number_of_users
 }
